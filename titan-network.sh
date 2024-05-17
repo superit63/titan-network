eddc3128f180b1a17729d7808ef569cb0f64126e
superit63 <superit63@gmail.com>
2024-05-17 17:10:33 +0700

    Update titan-network.sh

--- titan-network.sh
+++ titan-network.sh
@@ -1,16 +1,40 @@
 #!/bin/bash
 
+# Function to check if apt process is running
+function check_apt_process() {
+    while pgrep -x "apt" >/dev/null; do
+        echo "Killing other apt processes..."
+        sudo pkill -x apt
+        sleep 5
+    done
+}
+
+# Function to automatically reconfigure dpkg if it was interrupted
+function dpkg_configure_auto_yes() {
+    echo "Reconfiguring dpkg..."
+    sudo DEBIAN_FRONTEND=noninteractive dpkg --configure -a
+}
+
 # Node installation function
 function install_node() {
-    # Automatically update and upgrade packages
-    sudo apt update -y
-    sudo apt upgrade -y
+    # Check and kill other apt processes
+    check_apt_process
+
+    # Check if dpkg was interrupted
+    if [ -f /var/lib/dpkg/lock ]; then
+        echo "dpkg was interrupted. Reconfiguring..."
+        dpkg_configure_auto_yes
+    fi
+
+    # Automatically update and upgrade packages without prompting
+    sudo DEBIAN_FRONTEND=noninteractive apt update -y
+    sudo DEBIAN_FRONTEND=noninteractive apt upgrade -y -o Dpkg::Options::="--force-confnew" -o Dpkg::Options::="--force-confdef"
 
     # Check if Docker is installed
     if ! command -v docker &> /dev/null
     then
         echo "Installing Docker..."
-        sudo apt install -y ca-certificates curl gnupg lsb-release docker.io
+        sudo DEBIAN_FRONTEND=noninteractive apt install -y ca-certificates curl gnupg lsb-release docker.io
     else
         echo "Docker is already installed."
     fi
@@ -33,7 +57,7 @@ function install_node() {
         mkdir -p "$HOME/titan_storage_$i"
     
         # Start node
-        container_id=$(sudo docker run -d --restart always -v "$HOME/titan_storage_$i:/root/.titanedge/storage" --name "titan$i" --net=host  nezha123/titan-edge:1.5)
+        container_id=$(sudo docker run -d --restart always -v "$HOME/titan_storage_$i:/root/.titanedge/storage" --name "titan$i" --net=host nezha123/titan-edge:1.5)
         echo "Node titan$i started, container ID $container_id"
         sleep 30
     
